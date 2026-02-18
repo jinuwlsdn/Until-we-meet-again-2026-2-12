7acc59d755611ae2df5059c9921ddf14e11f5407
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-02-18 11:45:23 +0000

    11725 dfs / 3 not yet

--- main.cpp
+++ main.cpp
@@ -10,33 +10,22 @@
 
 using namespace std;
 
-const int MAX = 50;
-int w,h;
-int land[MAX+1][MAX+1];
+const int MAX = 1e5;
+
+int graph[MAX+1][MAX+1];
 bool visited[MAX+1][MAX+1];
-int dx[8] = {1,1,0,-1,-1,-1,0,1};
-int dy[8] = {0,1,1,1,0,-1,-1,-1};
 
-void dfs(int y, int x){
 
-  
-  for(int i = 0 ; i < 8; ++i ){
-    int ny = y+dy[i];
-    int nx = x+dx[i];
+void dfs(int k){
+  //k를 넣고 k의 부모를 찾아올라가서 반환하면 된다.
 
-    //섬이고 방문 X
 
-    
 
-    if(0<= ny && ny < h && 0<=nx && nx<w ){
-      //범위 안에 있으면
-      if(land[ny][nx] && !visited[ny][nx]){
-        visited[ny][nx] = true;
-        dfs(ny,nx);
-      }
-    }
-  }
+
 }
+
+
+
 
 
 int main(){
@@ -44,70 +33,37 @@ int main(){
   ios::sync_with_stdio(false);
   cin.tie(0);
 
+  //11725
 
   //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
+  //1. 이차원 배열로 서로 연결해서 부모를 찾는 방법으로 접근해야할 것 같다.
+  //그리고 당연히 방문한 노드는 다시 방문하지 않는다. 그리고 2번노드 부터 출력한다 
+  //탐색하려고 하는 대상 노드를 p라고 했을 때 graph[p][k] == 1 (k는 1부터 n) 
+  //result[k] = p , dfs(k) 끝나면 for문으로 1부터 n까지 출력.
 
-  //1. 2667번 문제랑 비슷한데 대각선까지도 섬으로 연결하는 부분이 추가됐네 그러면 이동할 수 있는 가짓수가 8
+  //2. 시간복잡도 O(V+E)  트리니까 정점은 N , E는 2*N 총 3*N 1억보다 작다.
 
-
-  //2. 시간복잡도 똑같이 O(V+E) = v에다가 이동할수있는 간선이 8개니까 9v  v는 n^2 2500은 1억보다 작다.
-
-  //3. 이차원배열 및 bool visited 2차원배열에서 탐색이니까 4가지 방향 생성해야한다.
+  //3. 관계를 나타낼 때에는 이차원배열로 표현 , bool visited <- 1차원배열.
   
 
-  //4 이차원배열의 값은 들어올때마다 초기화를 해줬는데 visited의 값은 초기화를 해주지 않았다.
-  //memset 사용
+  int n;
 
+  cin >> n;
 
-  //4963
+  for(int i = 2 ; i <= n ; ++i){
 
-  
-  
+    //여기서 2번노드부터니까 i는 1부터시작.
 
-  //너비가 w 높이가 h
-  while(1){
+    int p,q;
+    cin >> p >> q;
 
+    graph[p][q] = 1;
+    graph[q][p] = 1;
 
-    memset(visited, false, sizeof(visited));
-    memset(land , 0 , sizeof(land));
-
-    
-
-    int count = 0;
-    cin >> w >> h;
-    if(w == 0 && h == 0){
-      break;
-    }
-
-    for(int i = 0; i<h; ++i){
-      for(int j = 0 ; j<w ;++j){
-        
-        cin >> land[i][j];
-      }
-    }
-
-    
-    for(int i = 0; i<h; ++i){
-      for(int j = 0 ; j<w ;++j){
-        if(land[i][j] && !visited[i][j]){
-          //섬
-
-          visited[i][j] = true;
-          dfs(i,j);
-          count++;
-
-        }
-
-
-      }
-
-      
-    }
-
-
-    cout << count << '\n';
+    //이어져있다 둘이
   }
 
+  
 
   return 0;
 }
