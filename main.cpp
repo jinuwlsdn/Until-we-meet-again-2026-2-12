cf7aebbdc7c4e6732110c54eea8dcd3c8dd1f1f6
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-02-19 09:55:17 +0000

    2583 bfs/ 5 처음에 올린 것이 dfs

--- main.cpp
+++ main.cpp
@@ -14,33 +14,51 @@ bool visited[MAX+1][MAX+1];
 
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
+queue<pair<int ,int>> q;
 int m,n,k;
-
 
 //m은 세로 n은 가로
 
-int value = 0;
-void dfs(int y, int x){
 
+int bfs(int y, int x){
+
+  int value =0;
+  
+  q.push({y,x});
   visited[y][x] = true;
-  value++;
 
-  for(int i= 0 ; i < 4 ; ++i){
-    int ny = y + dy[i];
-    int nx = x + dx[i];
+  while(!q.empty()){
+    int y = q.front().first;
+    int x = q.front().second;
+    
 
+    value++;
+    q.pop();
 
-    if(0<= ny && ny < m && 0<= nx && nx < n){
-      //좌표값이 알맞고
+    for(int i = 0 ; i < 4 ; ++i){
+      int ny = y + dy[i];
+      int nx = x + dx[i];
 
-      if(!visited[ny][nx] && !paper[ny][nx]){
+      if(0<= ny && ny < m && 0 <= nx && nx < n){
+        //좌표값 부합하고
 
-        dfs(ny,nx);
+        if(!visited[ny][nx] && !paper[ny][nx]){
+
+          q.push({ny,nx});
+          visited[ny][nx] = true;
+          
+        }
       }
     }
+
   }
 
+
+  return value;
 }
+
+
+
 int main(){
 
   ios::sync_with_stdio(false);
@@ -50,15 +68,9 @@ int main(){
 
   //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
 
-  //1. 좌표를 받은 후에 그 좌표 값을 통해서 paper에 해당구역을 1로 바꾸고
-  //그 후에 dfs를 사용해서 넓이 값을 구하면 될 것 같다.
+  //BFS로 풀이
 
-  //2. 시간복잡도 O(V+E) 2차원배열에서 MAX값이 100일때 100*100 시간제한 1초보다 작다.
   
-  //3. 이차원배열 및 재귀함수 사용
-
-  //4. 가로 세로 구별 잘하기. visited true 해주기
-
 
  
   cin >> m >> n >> k;
@@ -82,23 +94,28 @@ int main(){
 
   }
 
-  //탐색하면서 dfs
+  //탐색하면서 bfs
 
 
-  
   for(int i = 0 ; i < m ; ++i){
     for(int j = 0 ; j < n ; ++j){
-      
-      if(!paper[i][j] && !visited[i][j]){
-        //해당 구역이 0이고 방문 X
 
-        value =0;
-        dfs(i,j);
-        area.push_back(value);
+      if(!visited[i][j] && !paper[i][j]){
+
+        int area_value = bfs(i,j);
+
+        area.push_back(area_value);
       }
     }
   }
+  
 
+  
+
+
+
+  
+  
   //둘째 줄에는 각 영역의 넓이를 오름차순으로 정렬하여 빈칸을 사이에 두고 출력한다.
 
   sort(area.begin(), area.end());
