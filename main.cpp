41c10ff406084598aac9d3b2ff5416f1735ee60f
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-02-19 09:35:59 +0000

    2583 bfs/5

--- main.cpp
+++ main.cpp
@@ -5,150 +5,111 @@
 #include <vector>
 #include <cstring>
 
-
-
-
 using namespace std;
 
 const int MAX = 100;
 
-int n;
-char painting[MAX+1][MAX+1];
+int paper[MAX+1][MAX+1];
 bool visited[MAX+1][MAX+1];
-
-int result[2];
 
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
+int m,n,k;
 
 
-void dfs1(int y, int x, char color){
+//m은 세로 n은 가로
+
+int value = 0;
+void dfs(int y, int x){
 
   visited[y][x] = true;
+  value++;
 
-  //적록색약이 아닌 경우 
-
-  for(int i = 0 ;  i<4 ; ++i){
-
+  for(int i= 0 ; i < 4 ; ++i){
     int ny = y + dy[i];
     int nx = x + dx[i];
 
-    if(0<= ny  && ny < n && 0 <= nx && nx < n){
-      //범위 안에 들어가고 
 
-      if(painting[ny][nx] == color && !visited[ny][nx]){
-        //색깔이 같고 방문하지않았으면 
+    if(0<= ny && ny < m && 0<= nx && nx < n){
+      //좌표값이 알맞고
 
-        
-        dfs1(ny,nx,color);
+      if(!visited[ny][nx] && !paper[ny][nx]){
+
+        dfs(ny,nx);
       }
     }
-
   }
 
 }
-
-
-
-
-
-
-
-
-
-
 int main(){
 
   ios::sync_with_stdio(false);
   cin.tie(0);
 
-  //10026
+  //2583
 
   //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
 
-  //1 RGB로 입력을 받으면 적록색약인 사람이 봤을때, 아닌 사람이 봤을때 차이만 구별해서
-  //코드를 완성하면 될 것 같다. 그러면 dfs함수를 2개를 작성해서 만들고 각각 값을 result[0]
-  //result[1]에 저장해서 출력하면 된다
+  //1. 좌표를 받은 후에 그 좌표 값을 통해서 paper에 해당구역을 1로 바꾸고
+  //그 후에 dfs를 사용해서 넓이 값을 구하면 될 것 같다.
 
-  //2. 시간복잡도 O(V+E) n이 100이므로 n^2의 값이 1초보다 작다.
-
-  //3. 재귀함수와 n이 100이므로 이차원배열을 사용해도 될 것 같다. 
-
-  //4. visited체크 
-
-
+  //2. 시간복잡도 O(V+E) 2차원배열에서 MAX값이 100일때 100*100 시간제한 1초보다 작다.
   
-  cin >> n;
+  //3. 이차원배열 및 재귀함수 사용
+
+  //4. 가로 세로 구별 잘하기. visited true 해주기
 
 
-  for(int i = 0 ; i < n ; ++i){
-    string line;
-    cin >> line;
+ 
+  cin >> m >> n >> k;
 
-    for(int j = 0 ; j<line.length(); ++j){
-      painting[i][j] = line[j];
-    }
-  }
+  //값을 저장할 저장공간 만들기
 
+  vector<int> area;
 
+  //세로가 m 가로가 n
 
-  //첫번째
-  
+  for(int i = 0 ; i < k ; ++i){
+    int x1,x2,y1,y2;
 
-  for(int i = 0 ; i < n ;++i){
-    for(int j = 0 ; j<n; ++j){
+    cin >> x1 >> y1 >> x2 >> y2;
 
-      if(!visited[i][j]){
-        result[0]++;
-
-        //방문하지않았으면 
-        dfs1(i,j,painting[i][j]);
+    for(int y = y1; y < y2; ++y){
+      for(int x = x1; x<x2 ; ++x){
+        paper[y][x] = 1;
       }
-
-
     }
+
   }
 
+  //탐색하면서 dfs
 
-  //조건에 "색상의 차이를 거의 느끼지 못하는 경우도 같은 색상이라 한다" 
-  //라는 말이 있으니까 G를 R로 바꾸고 dfs1을 쓰면 될 것 같다.
 
-  for(int i = 0 ; i < n ;++i){
-    for(int j = 0 ; j<n; ++j){
+  
+  for(int i = 0 ; i < m ; ++i){
+    for(int j = 0 ; j < n ; ++j){
+      
+      if(!paper[i][j] && !visited[i][j]){
+        //해당 구역이 0이고 방문 X
 
-      if(painting[i][j] == 'G'){
-        painting[i][j] = 'R';
+        value =0;
+        dfs(i,j);
+        area.push_back(value);
       }
-
     }
   }
 
-  
+  //둘째 줄에는 각 영역의 넓이를 오름차순으로 정렬하여 빈칸을 사이에 두고 출력한다.
+
+  sort(area.begin(), area.end());
 
 
-  
+  cout << area.size() << '\n';
 
-  memset(visited, false, sizeof(visited));
-  //두번째
-
-   for(int i = 0 ; i < n ;++i){
-    for(int j = 0 ; j<n; ++j){
-
-      if(!visited[i][j]){
-        result[1]++;
-
-        //방문하지않았으면 
-        dfs1(i,j,painting[i][j]);
-      }
-
-
-    }
+  for(int item : area){
+    cout << item << " ";
   }
   
-  
-
-
-  cout << result[0] << " " << result[1] << '\n';
 
 
   return 0;
