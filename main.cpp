6440d027bfbdc0b28db20d5d7551c4acb58bdf02
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-02-21 06:43:21 +0000

    2644 dfs / 5

--- main.cpp
+++ main.cpp
@@ -7,92 +7,75 @@
 
 using namespace std;
 
-const int MAX = 20;
 
-int soccer[MAX+1][MAX+1];
+const int MAX = 100;
+int people[MAX+1][MAX+1];
 bool visited[MAX+1];
-int n; 
-int min_gap = 1e9;
+int n;
+int t1, t2;
 
+int value  = -1;
 
-void dfs(int depth, int idx){
+void dfs(int depth, int num){
 
-  if(depth == n/2){
-    
-    int s_team =0;
-    int l_team =0;
-
-    for(int i = 1;  i<=n ; ++i){
-      for(int j = 1; j <=n ; ++j){
-
-        if(visited[i] && visited[j]){
-          //둘 다 같은 팀 true를 s팀이라고 하자
-          s_team += soccer[i][j];
-        }
-        else if(!visited[i] && !visited[j]){
-          //둘 다 같은 팀 false를 l팀이라고 하자
-          l_team += soccer[i][j];
-        }
-
-      }
-    }
-
-    int min_value = abs(s_team - l_team);
-    min_gap = min(min_gap, min_value);
+  if(num == t2){
+    value = depth;
     return;
   }
 
 
-
-  for(int i = idx  ; i <= n; ++i){
-
-    if(!visited[i]){
-      //방문 X 
+  for(int i = 1; i <= n; ++i){
+    if(people[num][i] && !visited[i]){
       visited[i] = true;
-      dfs(depth+1, i+1);
+      dfs(depth+1, i);
       visited[i] = false;
     }
   }
 }
-
 
 int main(){
 
   ios::sync_with_stdio(false);
   cin.tie(0);
 
-  //14889
+  //2644
 
   //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
 
-  //1. true , false로 팀 두개를 만든 다음에 depth ==  n/2이면 그 때 true false 값을 통해서
-  //팀의 능력치를 계산한다.
+  //1. 촌 계산을 하면 된다. dfs로 t1을 받아서 그 t1 t2가 만날때까지 dfs를 하면 정답을
+  //찾을 수 있다. 이 때 정답은 depth값이 된다.
 
-  //2. ..? N이 20이고 만드는 팀의 구성원은 N/2 최대가 10이니까 중복을 허용 X O(N!) N이 10이므로 가능
+  //2. 시간복잡도는 O(V+E) : V는 100 E는 간선이 2개니까 2 * 100 300은 1억보다 작다.
 
-  //3. 값을 저장할 이차원배열 , 또한 중복을 허용하지 않으므로 bool 1차원배열
+  //3. 두 부모 자식간의 관계를 나타내기 위해서 이차원배열을 사용, bool visited(갔다가 다시 돌아오면 안되기 때문에)
 
-  //4. dfs의 종료조건을 설정해야한다.
+  //4. bool visited , 종료조건 
 
-
-  
   cin >> n;
 
+  
 
-  //i가 세로 j가 가로
+  cin >> t1 >> t2;
 
-  for(int i = 1 ; i <= n ;  ++i){
-    for(int j = 1 ; j <= n ;  ++j){
-      cin >> soccer[i][j];
-    }
+  int m;
+
+  cin >> m;
+
+  for(int i =0 ; i  < m;  ++i){
+
+    int p,q;
+
+    cin >> p >> q;
+
+    people[p][q] = 1;
+    people[q][p] = 1;
+
   }
 
+  dfs(0,t1);
 
-  dfs(0,1);
-
-  cout << min_gap << '\n';
-
-  
+ 
+  cout << value << '\n';
 
   
  
