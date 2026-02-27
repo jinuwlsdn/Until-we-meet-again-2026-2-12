f4af33ec6346e5757ca9d4dc309f61680043e23f
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-02-27 14:05:28 +0000

    1806 투포인터 / 2

--- main.cpp
+++ main.cpp
@@ -16,51 +16,63 @@ int main(){
   cin.tie(0);
 
   
-  //3273
+  //1806
 
   //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
 
-  int n;
-  cin >> n;
+
+
+  //1. S 이상 되는 것 중 가장 짧은 것의 길이. 투포인터로 문제를
+  // 풀되 index 값으로 접근 하면 될 것 같다
+
+
+  //2 0.5초 5천만 N번만 탐색한다면 O(N) 이므로 1e5 가능.
+
+  //3 vector 백만 바이트가 1mb 40만바이트는 0.4mb 
+
+  //4 index값 조정 
+
+
+  int n,s;
+
+  cin >> n >> s;
+
   vector<int> v(n);
 
-  for(int i = 0 ; i < n ; ++i){
+  for(int i = 0 ; i <n ; ++i){
     cin >> v[i];
   }
+  // 부분합 중에 그 합이 S 이상이 되는 것 중,
 
-  int x;
-  cin >> x;
+  int start = 0;
+  //int end = 0;
+  int sum = 0;
+  int min_len = 1e7;
+  
 
-  sort(v.begin(), v.end());
+  
+  for(int end = 0 ; end < n ; ++end){
 
-  int cnt = 0;
+    sum += v[end];
+    
+    while(sum >= s){
+      min_len = min(min_len, end-start+1);
 
-  int start =0;
-  int end = n-1;
-
-  while(start < end){
-    int sum = v[start] + v[end];
-
-    if(sum == x){
-      cnt++;
-      start ++;
+      sum -= v[start++];
     }
-
-    if(sum > x){
-      end--;
-    }
-    else if(sum < x){
-      start++;
-    }
-
-
-
+    
 
   }
 
+  
+  if(min_len == 1e7) cout << 0 << '\n';
+  else cout << min_len << '\n';
+    
+  
+  
+  
 
-  cout << cnt << '\n';
-
+  
 
   
   return 0;
