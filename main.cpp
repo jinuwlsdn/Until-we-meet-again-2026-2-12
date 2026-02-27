80f879638f1d5698ff6db352ee48ef9271b118c0
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-02-27 13:08:38 +0000

    3273 투포인터 / 1

--- main.cpp
+++ main.cpp
@@ -16,59 +16,50 @@ int main(){
   cin.tie(0);
 
   
-  //14719
+  //3273
 
   //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
 
+  int n;
+  cin >> n;
+  vector<int> v(n);
 
-  //1. 이차원배열을 통해서 만약 블럭이 1이라면 그 세로선을 통해 가로로 탐색하면서
-  //다시 1이 되는 블럭을 찾고 그 블럭까지의 거리를 cnt에 추가한다 그리고 탐색한 그 블럭들은
-  //다시 중첩이 되면 안되기 때문에 1의 값을 다시 넣어준다.
+  for(int i = 0 ; i < n ; ++i){
+    cin >> v[i];
+  }
 
-  //2. n^2 모든 블럭들을 다 순회하고 그 블럭중에서 다시 가로로 탐색한다고 가정했을때 n번 더곱한다
-  //n^3
+  int x;
+  cin >> x;
 
-  //3 이차원배열
+  sort(v.begin(), v.end());
+
+  int cnt = 0;
+
+  int start =0;
+  int end = n-1;
+
+  while(start < end){
+    int sum = v[start] + v[end];
+
+    if(sum == x){
+      cnt++;
+      start ++;
+    }
+
+    if(sum > x){
+      end--;
+    }
+    else if(sum < x){
+      start++;
+    }
 
 
 
-  int h,w;
 
-  cin >> h>>w;
-
-  int cnt=0;
-
-  vector<int> height(w);
-
-  for(int i = 0 ; i < w ; ++i){
-    cin >> height[i];
   }
 
 
-  for(int i = 1; i<w-1; ++i){
-
-    int left_h =0;
-    int right_h =0;
-
-    for(int j = 0 ; j<i; ++j){
-      left_h = max(left_h, height[j]);
-    }
-    for(int j = i; j<w; ++j){
-      right_h = max(right_h, height[j]);
-    }
-
-    int min_h =0;
-
-    min_h = min(left_h, right_h);
-
-    if(min_h > height[i]){
-      cnt += (min_h - height[i]);
-    }
-
-  }
-  
   cout << cnt << '\n';
-
 
 
   
