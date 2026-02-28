f79c072876f2c48ed47da38704e2fd931f9c5318
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-02-28 10:33:01 +0000

    1759 백트래킹 / 7

--- main.cpp
+++ main.cpp
@@ -9,7 +9,48 @@
 
 using namespace std;
 
-typedef long long ll;
+
+vector<char> input_value;
+//bool visited[16];
+vector<string> result;
+int l,c;
+
+void recurse(int depth, int next ,string words){
+
+  if(depth == l){
+    //모음과 자음이 알맞게 들어갔는지 확인을해야한다.
+
+    int mo = 0;
+    int ja = 0;
+    //모는 1이상 자는 2이상이 되었을때 만족하는 words라서 그때 
+    //push_back하고 아니면 return한다.
+    for(int i = 0 ; i<words.length(); ++i){
+      if(words[i] == 'a' || words[i] == 'e' || 
+        words[i] == 'i' || words[i] == 'o' || words[i] == 'u' ){
+          mo++;
+        }
+      else{
+        ja++;
+      }
+    }
+
+    if(mo >= 1 && ja >= 2){
+      //result.push_back(words);
+      cout << words << '\n';
+    }
+    else return;
+    
+
+    
+  }
+
+
+  for(int i = next ; i<c; ++i){
+    recurse(depth+1, i+1, words + input_value[i]);
+  }
+
+
+}
 
 int main(){
 
@@ -18,73 +59,36 @@ int main(){
 
   
   
-  //2473
+  //1759
   //16234
   
   //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
 
-  int n;
-  cin >> n;
+  //1. 중복 X 서로다른 소문자  길이는 L 결과값에 넣을때 확인
 
-  vector<ll> v(n);
 
-  for(int i = 0 ; i<n; ++i){
-    cin >> v[i];
+  
+
+  cin >> l >> c;
+
+  input_value.resize(c);
+  for(int i = 0 ; i < c ; ++i){
+    cin >> input_value[i];
   }
 
-  sort(v.begin(), v.end());
+  sort(input_value.begin(), input_value.end());
+  
 
-  ll min_diff = 3e10;
-  vector<ll> ans(3);
-
-  for(int i = 0 ; i < n ; ++i){
-    //i로 돌아가면서 모든 상황에 대해서 계산
-
-    int l = 0 , r = n-1;
-
-    while(l<r){
-
-      if(l==i){
-        ++l;
-        continue;
-      }
-      if(r==i){
-        --r;
-        continue;
-      }
-
-      ll sum = v[i] + v[l] + v[r];
-
-      if(min_diff > abs(sum)){
-        
-        min_diff = abs(sum);
-        ans[0] = v[i];
-        ans[1] = v[l];
-        ans[2] = v[r];
-
-        if(sum == 0) break;
-      }
-
-      if(sum < 0){
-        ++l;
-      }
-      else{
-        --r;
-      }
-    }
+  recurse(0,0,"");
 
 
 
-  }
 
+  // sort(result.begin(), result.end());
 
-
-  sort(ans.begin(), ans.end());
-
-  for(int k  : ans){
-    cout << k << " ";
-  }
-
+  // for(string ans : result){
+  //   cout << ans << '\n';
+  // }
 
   return 0;
 }
