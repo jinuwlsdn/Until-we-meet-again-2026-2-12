8688a5bc193b88e26d7179b1092e6f3a5f9b38d3
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-02-28 02:06:14 +0000

    1644 투포인터 / 3

--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@
 #include <vector>
 #include <cstring>
 #include <set>
+#include <math.h>
 
 using namespace std;
 
-
+const int MAX = 4000000;
 
 int main(){
 
@@ -16,63 +17,77 @@ int main(){
   cin.tie(0);
 
   
-  //1806
+  //1644
 
   //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
 
 
 
-  //1. S 이상 되는 것 중 가장 짧은 것의 길이. 투포인터로 문제를
-  // 풀되 index 값으로 접근 하면 될 것 같다
+  //1 체를 써서 소수만 쓰고 그 다음에 연속된 소수의 합으로 나타내야하는데 어떻게 하면
+  //연속되게 할 수 있을까..
 
 
-  //2 0.5초 5천만 N번만 탐색한다면 O(N) 이므로 1e5 가능.
+  //2 O(N)만 돈다고 하면 4000000
 
-  //3 vector 백만 바이트가 1mb 40만바이트는 0.4mb 
+  //3 백만 바이트가 1mb 16mb 
 
   //4 index값 조정 
 
+  int n;
 
-  int n,s;
+  cin >> n;
 
-  cin >> n >> s;
+  vector<bool> isprime(MAX+1, true);
+  vector<int> prime_num(283146+1);
 
-  vector<int> v(n);
+  //0과 1은 소수가 아니다.
+  isprime[0] = isprime[1] = false;
 
-  for(int i = 0 ; i <n ; ++i){
-    cin >> v[i];
-  }
-  // 부분합 중에 그 합이 S 이상이 되는 것 중,
+  int idx =0;
 
-  int start = 0;
-  //int end = 0;
-  int sum = 0;
-  int min_len = 1e7;
-  
+  for(int i = 2; i <= sqrt(MAX); ++i){
+    if(isprime[i]){
 
-  
-  for(int end = 0 ; end < n ; ++end){
-
-    sum += v[end];
-    
-    while(sum >= s){
-      min_len = min(min_len, end-start+1);
-
-      sum -= v[start++];
+      for(int j = i*i ; j <= MAX ; j+=i){
+        isprime[j] = false;
+      }
     }
-    
+  }
 
+  for(int i = 2; i <= MAX ; ++i){
+    if(isprime[i]) prime_num[idx++] = i;
   }
 
   
-  if(min_len == 1e7) cout << 0 << '\n';
-  else cout << min_len << '\n';
-    
-  
-  
-  
+  int cnt = 0;
 
+  int start =0;
   
+  int sum = 0;
+ 
+  int end = 0;
+
+  int prime_count = idx;
+
+  while(true){
+    if(sum >= n){
+      if(sum == n) cnt++;
+      sum -= prime_num[start++];
+    }
+    else if(end == prime_count){
+      break;
+    }
+    else{
+      //합이 n보다 작다
+      sum += prime_num[end++];
+    }
+
+  }
+
+
+ 
+
+  cout << cnt << '\n';
 
   
   return 0;
