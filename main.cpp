eddf04c792fc81f9cc3369808111b77551f502e0
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-03-03 11:20:04 +0000

    7576 bfs / 11

--- main.cpp
+++ main.cpp
@@ -9,8 +9,12 @@
 
 using namespace std;
 
-vector<int> v[101];
-vector<int> result[101];
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
+int tomato[1001][1001];
+//int result[1001][1001];
+bool visited[1001][1001];
 
 
 int main(){
@@ -20,74 +24,108 @@ int main(){
 
   
   
-  //1389
+  //
   //16234
   
   //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
 
 
-  int n,m;
+  //1 문제에서 시키는대로 구현만 하면 될 것 같다 근데 한 중심으로
+  //퍼지는 부분이니까 bfs를 사용하면 될 것 같고 그래프에다가 값을 넣어서 !q.empty() 될때까지
+  //작업을 시행한다 그리고 그래프에서 값을 탐색하면서 만약 0이 있다면 -1이고  모두 익어있으면
+  //0을 출력하고 아니면 최소날짜기때문에 그래프중에서 가장 높은 값을 선택하면된다.
+  //그래프 탐색하면서 값을 줄때 현재값 + 1을 주면 다음날로 저장이 된다.
 
-  cin >> n >> m;
+  //2. O(V+E) 1000 * 1000 + 4 * 1000 * 1000 1억보다 작다.
 
-  for(int i = 0 ; i < m ;  ++i){
-    int p,q;
-    cin >> p >> q;
+  //3. 이차원배열 및 visited
+  // 1000 * 1000 * 4 = 4000000 백만바이트 4mb 충분하다
 
-    v[p].push_back(q);
-    v[q].push_back(p);
+  //visited 안하는거
+
+
+  //1은 익은 토마토 0은 익지않은 토마토 -1은 들어있지 않은 칸
+
+  int m,n; 
+  //m은 가로 n은 세로
+
+  cin >> m >> n;
+
+   queue<pair<int,int>> q;
+
+  for(int i = 0 ; i <n ; ++i){
+    for(int j = 0 ; j < m ; ++j){
+      cin >> tomato[i][j];
+      if(tomato[i][j] == 1){
+        visited[i][j]= true;
+        q.push({i,j});
+      }
+    }
   }
-  
 
-  
-  for(int i=1 ; i<=n; ++i){
-    queue<pair<int, int>> q;
-    
-    
-    bool visited[101];
-    memset(visited, false, sizeof(visited));
-    q.push({i,0});
-    visited[i] = true;
 
-    while(!q.empty()){
+  while(!q.empty()){
 
-      int person = q.front().first;
-      int depth = q.front().second;
+    int y = q.front().first;
+    int x = q.front().second;
 
-      q.pop();
+    q.pop();
 
-      for(int next : v[person]){
+    for(int i = 0 ; i<4 ; ++i){
+      int nx = x+dx[i];
+      int ny = y+dy[i];
 
-        if(!visited[next]){
-          visited[next] =true;
-          q.push({next, depth+1});
-          result[i].push_back(depth);
+      if(0<=nx && nx < m && 0<=ny && ny < n){
+        //범위안에 들어오고
+
+        if(!visited[ny][nx] && tomato[ny][nx] != -1){
+          visited[ny][nx] = true;
+          tomato[ny][nx] = tomato[y][x] + 1;
+          q.push({ny,nx});
         }
       }
-
-    }
-
-  }
-
-  int min_value  =  5000000;
-  int min_person = 0;
-  for(int i = 1; i <= n ; ++i){
-    int sum = 0;
-
-    for(int value : result[i]){
-      sum += value;
-    }
-
-    if(min_value > sum){
-      min_value = sum;
-      min_person = i;
     }
   }
+  
+  //값 확인
+  //저장될 때부터 모든 토마토가 익어있는 상태이면 0을 출력 이 경우는 최댓값이 1
 
+  // cout << '\n';
 
-  cout << min_person << '\n';
+  // for(int i = 0 ; i < n ; ++i){
+  //   for(int j = 0 ; j < m ; ++j){
+  //     cout << tomato[i][j] << " ";
+  //   }
+  //   cout << '\n';
+  // }
 
+  int min_day = 0;
+  bool check = false;
 
+  for(int i= 0 ; i <n; ++i){
+    if(check) break;
+    for(int j=0 ; j<m ; ++j){
+      //아무것도들어있지 않은거 패스
+      if(tomato[i][j] == -1) continue;
+      //익지않은거 한개라도 들어있으면
+      if(tomato[i][j] == 0){
+        min_day = 0;
+        check = true;
+        break;
+      }
+      min_day = max(min_day , tomato[i][j]);
+    }
+  }
+  if(min_day == 1){
+    cout << 0 << '\n';
+  }
+  else if(min_day == 0){
+    cout << -1 << '\n';
+  }
+  else if(min_day > 1){
+    cout << min_day-1 << '\n';
+  }
+ 
 
   return 0;
 }
