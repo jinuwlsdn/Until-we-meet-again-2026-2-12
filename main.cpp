78dbe6d3973a5fddf49596f81a2cbc85441ca4e1
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-03-04 11:42:43 +0000

    16953 bfs / 12

--- main.cpp
+++ main.cpp
@@ -9,13 +9,12 @@
 
 using namespace std;
 
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
+const int MAX = 1e9;
 
-int tomato[1001][1001];
-//int result[1001][1001];
-bool visited[1001][1001];
 
+
+
+//int depth[MAX+1];
 
 int main(){
 
@@ -24,108 +23,58 @@ int main(){
 
   
   
-  //
+  //16953
   //16234
   
   //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
 
+  //10억 = 10^9
 
-  //1 문제에서 시키는대로 구현만 하면 될 것 같다 근데 한 중심으로
-  //퍼지는 부분이니까 bfs를 사용하면 될 것 같고 그래프에다가 값을 넣어서 !q.empty() 될때까지
-  //작업을 시행한다 그리고 그래프에서 값을 탐색하면서 만약 0이 있다면 -1이고  모두 익어있으면
-  //0을 출력하고 아니면 최소날짜기때문에 그래프중에서 가장 높은 값을 선택하면된다.
-  //그래프 탐색하면서 값을 줄때 현재값 + 1을 주면 다음날로 저장이 된다.
+  int a,b;
 
-  //2. O(V+E) 1000 * 1000 + 4 * 1000 * 1000 1억보다 작다.
+  cin >> a >> b;
 
-  //3. 이차원배열 및 visited
-  // 1000 * 1000 * 4 = 4000000 백만바이트 4mb 충분하다
+  queue<pair<long long, int>> q;
 
-  //visited 안하는거
+  set<long long> visited;
 
+  q.push({a,1});
+  visited.insert(a);
 
-  //1은 익은 토마토 0은 익지않은 토마토 -1은 들어있지 않은 칸
-
-  int m,n; 
-  //m은 가로 n은 세로
-
-  cin >> m >> n;
-
-   queue<pair<int,int>> q;
-
-  for(int i = 0 ; i <n ; ++i){
-    for(int j = 0 ; j < m ; ++j){
-      cin >> tomato[i][j];
-      if(tomato[i][j] == 1){
-        visited[i][j]= true;
-        q.push({i,j});
-      }
-    }
-  }
+  //dfs를 쓰면 최솟값이 아닐 수 가 있다 dfs는 한 곳만 파기때문에 
+  //그래서 퍼져나가는 원하는 수 b에 먼저 도달하는 친구가 있다면 그 친구가 최솟값이다.
+  //근데 dfs는 그럴 것이라는 보장이 없다.
 
 
+  int min_value = -1;
   while(!q.empty()){
 
-    int y = q.front().first;
-    int x = q.front().second;
+    long long curr = q.front().first;
+    int dist = q.front().second;
+
+    
+
+    if(curr == b){
+      min_value = dist;
+      break;
+    }
 
     q.pop();
 
-    for(int i = 0 ; i<4 ; ++i){
-      int nx = x+dx[i];
-      int ny = y+dy[i];
-
-      if(0<=nx && nx < m && 0<=ny && ny < n){
-        //범위안에 들어오고
-
-        if(!visited[ny][nx] && tomato[ny][nx] != -1){
-          visited[ny][nx] = true;
-          tomato[ny][nx] = tomato[y][x] + 1;
-          q.push({ny,nx});
-        }
-      }
+    if(!visited.count(curr * 2) && curr * 2 <= b){
+      q.push({curr * 2 , dist+1});
+      visited.insert(curr * 2);
+    }
+    long long next_num = curr*10 + 1;
+    if(next_num > MAX) continue;
+    if(!visited.count(next_num) && next_num <= b){
+      q.push({next_num, dist+1});
+      visited.insert(next_num);
     }
   }
-  
-  //값 확인
-  //저장될 때부터 모든 토마토가 익어있는 상태이면 0을 출력 이 경우는 최댓값이 1
 
-  // cout << '\n';
+  cout << min_value << '\n';
 
-  // for(int i = 0 ; i < n ; ++i){
-  //   for(int j = 0 ; j < m ; ++j){
-  //     cout << tomato[i][j] << " ";
-  //   }
-  //   cout << '\n';
-  // }
-
-  int min_day = 0;
-  bool check = false;
-
-  for(int i= 0 ; i <n; ++i){
-    if(check) break;
-    for(int j=0 ; j<m ; ++j){
-      //아무것도들어있지 않은거 패스
-      if(tomato[i][j] == -1) continue;
-      //익지않은거 한개라도 들어있으면
-      if(tomato[i][j] == 0){
-        min_day = 0;
-        check = true;
-        break;
-      }
-      min_day = max(min_day , tomato[i][j]);
-    }
-  }
-  if(min_day == 1){
-    cout << 0 << '\n';
-  }
-  else if(min_day == 0){
-    cout << -1 << '\n';
-  }
-  else if(min_day > 1){
-    cout << min_day-1 << '\n';
-  }
- 
 
   return 0;
 }
