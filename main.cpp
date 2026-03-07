8f9d97d2c44ae78234121989f4a5a28e24963515
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-03-07 10:15:58 +0000

    11663 이분탐색 / 3

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 
 using namespace std;
 
+vector<int> a;
+vector<int> b;
 
 
 int main(){
@@ -18,65 +20,73 @@ int main(){
 
   
   
-  //7795
+  //11663
   //16234
-  //2343
+  
   
   //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
 
+  //1. 입력 받고 점의 좌표를 정렬한다 그리고 이분탐색으로 접근하는데 index로 각 선분마다
+  //몇개 점이있는지 출력한다
 
-  int T;
-  cin >> T;
+  //2. nlogn 100000*20 <- 정렬했을때 , 이분탐색했을때 같다 100000*20 * 2 4백만 
+  //1억보다 작다.
 
-  while(T--){
+  //3. vector<int> 사용
 
-    int n,m;
+  int n,m;
 
-    cin >> n >> m;
+  cin >> n >> m;
 
-    vector<int> a;
-    vector<int> b;
-    
-
-    for(int i =0  ; i <n ; ++i){
-      int k;
-      cin >> k;
-      a.push_back(k);
-    }
-    for(int i =0  ; i <m ; ++i){
-      int k;
-      cin >> k;
-      b.push_back(k);
-    }
-    sort(b.begin(), b.end());
-
-    int count = 0;
-    for(int i =0  ; i <n ; ++i){
-      
-
-
-      int left = 0 , right = m;
-      while(left+1 < right){
-        int mid = (left+right)/2;
-
-        if(b[mid] < a[i]){
-          left = mid;
-        }
-        else right = mid;
-      }
-
-      count += left;
-      if(a[i] > b[left]) count++;
-      //1 3 6 
-      //8을 탐색해보는데 left가 이 경우엔 2다
-      //근데 86 83 81 3개이므로 count++ 해준다
-    }
-    
-    cout << count << '\n';
-
+  vector<int> dots;
+  for(int i = 0 ; i < n ; ++i){
+    int k;
+    cin >> k;
+    dots.push_back(k);
   }
 
-  
+  sort(dots.begin(), dots.end());
+  for(int i = 0 ; i < m ; ++i){
+    int st , en;
+    cin >> st >> en;
+
+    int count = 0, left = 0 , right = n-1;
+
+
+    while(left<=right){
+      int mid = (left+right) / 2;
+
+
+    
+      if(dots[mid] >= st){
+        right = mid - 1;
+      }
+      else{
+        left = mid + 1;
+      }
+
+    }
+    int underIndex = left;
+    left = 0 , right = n-1;
+
+    while(left<=right){
+      int mid = (left+right) / 2;
+
+      if(dots[mid] > en){
+        right = mid - 1;
+      }
+      else{
+        left = mid + 1;
+      }
+    }
+    int overIndex = right+ 1;
+
+
+    count = overIndex - underIndex;
+    cout << count << '\n';
+  }
+
+
 
   return 0;
 }
