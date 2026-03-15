c4734426fdb973427041597076cdb07745c0f7f6
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-03-15 09:30:00 +0000

    9184 DP / 2

--- main.cpp
+++ main.cpp
@@ -14,10 +14,38 @@ using namespace std;
 typedef long long ll;
 
 
+//값  k가 <= 0 이면 1 그니까 0이거나 음수면 1로 고정 
+//20보다 크면 20 20 20 으로 고정
+//결국 어떤 값 w(a,b,c)는 무수히 많은 1을 더하는 것이니까
+//초기값은 0으로 설정하고 만약 0이 아니면 한번 다녀왔다는것이다
+int arr[21][21][21]; 
+
+int w(int a, int b , int c){
+  if(a <= 0 || b <= 0 || c <= 0) return 1;
+  if(a > 20 || b > 20 || c> 20) return w(20,20,20);
+
+
+  //다녀왔다.
+  if(arr[a][b][c] != 0) return arr[a][b][c];
+
+
+  //안 다녀옴
+  if(a<b && b<c){
+    arr[a][b][c] = w(a,b,c-1) + w(a, b-1 ,c-1) - w(a,b-1,c);
+  }
+  else{
+    arr[a][b][c] =  w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+  }
+
+  return arr[a][b][c];
+
+
+}
+
 int main(){
 
-  ios::sync_with_stdio(false);
-  cin.tie(0);
+  // ios::sync_with_stdio(false);
+  // cin.tie(0);
 
 
   //1912
@@ -25,36 +53,18 @@ int main(){
   
   //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
 
+  int a,b,c;
+  while(1){
+    
 
-  //1. 연속된 몇개의 수를 더하는데 음수가 나오면 무조건 값이 원래 더한것보다 작아진다.
-  //전에 연속되는 수를 더하다가 특정한 조건(음수)이 나오면 다시 합을 만든다.
+    cin >> a >> b >> c;
 
-  //2. n은 십만이하 일일이 하나씩 더하면 시간초과 따라서 DP로 풀어야한다.
+    if(a == -1 && b == -1 && c == -1) break;
 
-  //3. 주어지는 수가 -1000 ~ 1000 따라서 int , 배열로 받아야한다.
-
-
-  int n;
-  cin >> n;
-  vector<int> seq(n);
-
-  for(int i = 0 ; i < n ; ++i){
-    cin >> seq[i];
+    //실행
+    printf("w(%d, %d, %d) = %d\n", a,b,c, w(a,b,c));
   }
-
-
-  int sum = seq[0]; 
-  int max_value = seq[0];
-  for(int i = 1 ; i < n ; ++i){
-    sum = max(seq[i], seq[i] + sum);
-
-    if(sum > max_value) max_value = sum;
-  }
-
   
-  cout << max_value << '\n';
-
-
   
 
 
