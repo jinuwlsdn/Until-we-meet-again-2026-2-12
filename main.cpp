45c449c9a00389c12fc3cb6a283a395116dc1477
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-03-21 08:30:00 +0000

    1647 MST / 2

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@ typedef long long ll;
 
 typedef pair<int , int> pii;
 
-vector<pii> adj[1001];
-bool visited[1001];
+vector<pii> adj[100001];
+bool visited[100001];
 
 int main(){
 
@@ -26,39 +26,53 @@ int main(){
 
   
   
-  //1197
+  //1647
   
   //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
 
   
-  int v,e;
+  int n, m;
 
-  cin >> v >> e;
-  for(int i=0 ; i <e; ++i){
-    int u,v,w;
-    cin >> u >> v >>w;
+  cin >> n >> m;
+  
+  int a,b,c;
 
-    adj[u].push_back({w,v});
-    adj[v].push_back({w,u});
+  for(int i = 0 ; i < m; ++i){
+    cin >> a >> b >> c;
+
+    adj[a].push_back({c,b});
+    adj[b].push_back({c,a});
   }
 
-  ll totalweight = 0;
-  //가중치가 int범위를 넘을 수 있다.
 
   priority_queue<pii, vector<pii>, greater<pii>> pq;
 
-  pq.push({0,1});
+  
 
+  //1억 이하
+  int sum =0;
+  int max_value =0;
+
+  //마을을 분리하고 각각 분리된 마을에서도 경로가 항상 존재하게 하도록 하면서
+  //나머지 길의 유지비의 합을 최소로 하고 싶다.
+  //mst를 만들고 거기서 가장 유지비가 높은 간선 하나를 제거하면 된다.
+  //
+
+  pq.push({0,1});
+  int count =0;
   while(!pq.empty()){
-    int w = pq.top().first;
+    int w  = pq.top().first;
     int curr = pq.top().second;
 
     pq.pop();
 
     if(visited[curr]) continue;
 
+    //처음 방문했으면
     visited[curr] = true;
-    totalweight += w;
+    sum += w;
+    max_value = max(max_value, w);
+    count++;
 
     for(auto& edge : adj[curr]){
       int nextweight = edge.first;
@@ -70,13 +84,15 @@ int main(){
 
     }
 
-    
+    //방문한 정점이 n개면 굳이 더 갈 필요가 없다 이미 mst를 만들어서
+    if(count == n) break;
+
   }
 
-
-
-  cout << totalweight << '\n';
-
-
+  
+  
+  cout << sum - max_value << '\n';
+  
+  
   return 0;
 }
