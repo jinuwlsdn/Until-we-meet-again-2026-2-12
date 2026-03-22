d9ff3f974aea58fd063f885ed0f5ae478da4e438
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-03-22 04:34:47 +0000

    데이크스트라 1504 / 4

--- main.cpp
+++ main.cpp
@@ -16,10 +16,41 @@ typedef long long ll;
 
 typedef pair<int, int> pii;
 
-vector<pii> adj[20001];
-int dist[20001];
+vector<pii> adj[801];
+int dist[801][801];
 
 const int INF = 1e9;
+
+void dijkstra(int start){
+
+  priority_queue<pii, vector<pii>, greater<pii>> pq;
+
+  dist[start][start] = 0;
+
+  pq.push({0,start});
+  
+  while(!pq.empty()){
+    int d = pq.top().first;
+    int curr = pq.top().second;
+
+    pq.pop();
+
+    if(dist[start][curr]  < d ) continue;
+
+    dist[start][curr] = d;
+
+    for(auto& edge : adj[curr]){
+      int w = edge.first;
+      int way = edge.second;
+
+      if(dist[start][way] > d+w){
+        dist[start][way] = d+w;
+        pq.push({dist[start][way], way});
+      }
+    }
+
+  }
+}
 
 int main(){
 
@@ -29,68 +60,51 @@ int main(){
 
   
   
-  //1446
+  //1504
   
   //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
 
-  //지름길이 있는건 지름길을 사용하고 없으면 그냥가야한다.
-  //만약에 0->30을 갔다고 하자 근데 29>100 직빵으로 가는 길이 있으면
-  //지나칠수있다. dist++하면서 
+  //1.
 
-  int n,d;
+  int n,e;
 
-  cin >> n >> d;
-
-
+  cin >> n >> e;
   
-  for(int i = 0 ; i<d ; ++i){
-    adj[i].push_back({1,i+1});
-  }
-
-  for(int i = 0 ; i < n; ++i){
+  for(int i = 0 ; i < e ; ++i){
     int a,b,c;
 
     cin >> a >> b >> c;
 
-    if(b>d) continue;
-
     adj[a].push_back({c,b});
-  }
-
-  dist[0] = 0;
-
-  priority_queue<pii, vector<pii> , greater<pii>> pq;
-  
-  pq.push({0,0});
-
-  fill(dist, dist+d+1, INF);
-
-  while(!pq.empty()){
-    int d = pq.top().first;
-    int curr = pq.top().second;
-
-    pq.pop();
-
-    if(dist[curr] < d ) continue;
-
-    dist[curr] = d;
-
-    for(auto& edge : adj[curr]){
-      int next_w = edge.first;
-      int next_way = edge.second;
-
-      if(dist[next_way] > next_w + d){
-        dist[next_way] = next_w + d;
-        pq.push({dist[next_way], next_way});
-      }
-    }
-
-
+    adj[b].push_back({c,a});
   }
 
 
-  cout << dist[d] << '\n';
-  
+  fill(&dist[0][0], &dist[n][n+1], INF);
+
+  int v1,v2;
+  cin >> v1 >> v2;
+
+  dijkstra(1);
+  dijkstra(v1);
+  dijkstra(v2);
+
+  ll case1 = (ll)dist[1][v1] + dist[v1][v2] + dist[v2][n];
+  ll case2 = (ll)dist[1][v2] + dist[v2][v1] + dist[v1][n];
+
+  if(case1 >= INF && case2 >= INF){
+    cout << -1 << '\n';
+    return 0;
+  }
+
+  if(case1 > case2){
+    cout << case2 << '\n';
+  }
+  else{
+    cout << case1 << '\n';
+  }
+
+
 
   return 0;
 }
