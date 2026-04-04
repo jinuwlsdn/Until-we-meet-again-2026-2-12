6499e7ade96a42ea682fd67d193c0e131cd73b00
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-04-04 06:12:12 +0000

    2458 플로이드  / 5

--- main.cpp
+++ main.cpp
@@ -18,61 +18,77 @@
 
   const int INF = 1e9;
 
-  int item[101];
-  int graph[101][101];
-  bool visited[101];
+  
 
   int main(){
 
     // ios::sync_with_stdio(false);
     // cin.tie(0);
 
+     
 
     
-    //14938
-    
-    
+    //2458
+
     //1 아이디어 2 시간복잡도 3 자료구조 4 유의해야할 점
+    
+    //1.본인한테 간선이 들어오거나 간선이 다른 정점을 향해 뻗어야 하는데 그 간선들의 수를 세보았을 때
+    // n-1값이면 정확하게 본인의 위치를 알 수 있다.
 
-    //1 플로이드 알고리즘을 쓰는데 이 때 본인을 0이 아닌 INF로 설정해서 graph i i 값도
-    //만들어야한다. graph[i][i]가 0이면 어떤값을 넣어도 갱신이 될 수 없다.
-    //자기 자신으로 돌아와야 사이클을 만들 수 있고 최솟값 갱신이 되어야 답을 구할 수 있다.
+    //이 문제가 왜 플로이드 알고리즘 -> m의 값이 n(n-1)/2로 주어진다는건 단방향 그래프라는 것이고
+    //그래프를 통해서 각각의 정점들이 문제의 조건에 해당하는 정점인지 확인해야한다.
+    //도달 가능 여부를 체크하기 위해
 
-    //플로이드 알고리즘을 사용할 경우 시간 복잡도는 O(N^3) 400의 3제곱
-    // 64 000 000
 
-    //3 2차원배열의 vector를 사용, 출력값의 최댓값 400 * 10000 ?
+    //The reason why I miss you lies in your smile
 
-    int v,e;
-    cin >> v >> e;
+    int n,m;
+    cin >> n >> m;
 
-    vector<vector<int>> graph(v+1, vector<int>(v+1,INF));
+    vector<vector<int>> students(n+1, vector<int>(n+1,INF));
 
-    for(int i = 0 ;  i < e ; ++i){
-      int a,b,c;
-      cin >> a >> b >> c;
-
-      //a->b
-      graph[a][b] = c;
+    for(int i = 1; i <= n ; ++i){
+      students[i][i] = 0;
     }
 
-    int min_cycle =INF;
-    for(int k =1 ; k <=v ;++k){
-      for(int i =1 ; i<=v; ++i){
-        for(int j = 1; j <=v ; ++j){
-          graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+    for(int i = 0 ; i < m ; ++i){
+      int a,b;
+      cin >> a >> b;
+      
+      students[a][b] = 1;
+
+    }
+
+
+    //플로이드 알고리즘
+    for(int k = 1; k <= n ; ++k){
+      for(int i = 1 ; i<=n ; ++i){
+        for(int j=1 ; j<=n; ++j){
+          students[i][j] = min(students[i][j], students[i][k] + students[k][j]);
         }
-        
       }
     }
+    
 
-    for(int i = 1; i<=v ; ++i){
-      min_cycle = min(min_cycle, graph[i][i]);
+    int total = 0;
+    for(int i = 1; i <=n ; ++i){
+      int count  =0;
+
+      for(int j =1 ; j <=n ; ++j){
+        if(i == j ) continue;
+        if(students[i][j] < INF || students[j][i] < INF){
+          // i->j 갈 수 있거나 j->i 쪽으로 들어 오거나
+          //존재한다면
+          count++;
+        }
+      }
+
+      if(count == n-1) total++;
     }
 
-    if(min_cycle == INF) cout << -1 << '\n';
-    else cout << min_cycle << '\n';
 
-
+    cout << total << '\n';
+    
+   
     return 0;
   }
