3da542e90e56efbac65802c0498d2725ea2d1e8b
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2026-05-07 11:41:23 +0000

    오일러 14

--- main.cpp
+++ main.cpp
@@ -21,23 +21,6 @@
   const int MAX = 2000000;
 
 
-  bool prime[MAX+1];
-
-
-  void sieve(){
-    prime[1] = false;
-
-    for(int i =2 ; i <=MAX;++i) prime[i] = true;
-
-
-
-    for(int i =2 ; i*i<=MAX; ++i){
-      for(int k = 2; i*k<=MAX; ++k){
-        if(!prime[i]) continue;
-        prime[i*k] = false;
-      }
-    }
-  }
   
  
   
@@ -48,45 +31,30 @@
     cin.tie(0);
 
     
-    sieve();
+    int max_count =0;
+    int answer = 0;
+
+    for(long long i = 1 ; i <= 1000000; ++i){
+      int count = 0;
 
 
-    for(long long i = 1; ; ++i){
-      
-      long long num = i*(i+1) / 2;
-      long long result_num = num;
+      long long num = i;
 
-      int result = 1;
-      for(int k = 2; k*k<=num; ++k){
-        if(prime[k] ==  true){
-          //k가 소수다.
-          if(num % k == 0){
-            
-            int count = 0;
-            while(num%k == 0){
-              count++;
-              num /= k;
-            }
-
-            result *= (count+1);
-          }
-        }
+      while(num != 1){
+        if(num % 2 == 0) num /= 2;
+        else num = 3*num + 1;
+        count++;
       }
 
-      //ex) 만약 7이 num이라고 하자
-      //k*k<=num이니까 2와 3은 나누지 못하고 그냥 for문이 끝나버린다.
-      //이때 result가 그냥 1로 저장되는데 7의 약수는 1과 7 이므로 원하는 답과 다르다.
-      //소수분해후 남은수가 있다면 그 자체도 소수다.
-      if(num > 1) result *= 2;
 
-      if(result >= 500){
-        cout << result_num <<'\n';
-        break;
+      if(count > max_count){
+        max_count = count;
+        answer = i;
       }
-      //cout << result << '\n';
     }
     
     
+    cout << answer << '\n';
    
     return 0;
   }
